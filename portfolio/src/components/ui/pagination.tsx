a80edd818c0fd6a2def61281904a9096a61ab7f0
saiavinashpasumarthy <saiavinashpasumarthy2005@gmail.com>
2026-08-14 11:26:44 +0530

    fix: set default size for PaginationPrevious and PaginationNext components

--- portfolio/src/components/ui/pagination.tsx
+++ portfolio/src/components/ui/pagination.tsx
@@ -71,9 +71,9 @@ const PaginationPrevious = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to previous page"
-    size="default"
     className={cn('gap-1 pl-2.5', className)}
     {...props}
+    size="default"
   >
     <ChevronLeft className="h-4 w-4" />
     <span>Previous</span>
@@ -87,9 +87,9 @@ const PaginationNext = ({
 }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink
     aria-label="Go to next page"
-    size="default"
     className={cn('gap-1 pr-2.5', className)}
     {...props}
+    size="default"
   >
     <span>Next</span>
     <ChevronRight className="h-4 w-4" />
